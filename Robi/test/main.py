6c156b30f93ecfbc434794dc1ebd57c5f83e623c
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-10-22 19:15:42 +0000

    final

--- Robi/test/main.py
+++ Robi/test/main.py
@@ -6,23 +6,24 @@ pet2 = Pet("Buddy", "Golden retriever with a wagging tail")
 pet3 = Pet("Bugs", "Fluffy gray and white rabbit")
 pet4 = Pet("Felix", "Black and white mischievous cat")
 
-
-owner1 = Owner("Hans", 30, True)
-owner2 = Owner("John", 25, False)
-
+owner1 = Owner("Hans", 24, True)
+owner2 = Owner("John", 30, False)
 
 owner1.petsOwned = [pet1, pet2, pet3]
 owner2.petsOwned = [pet4]
 
-
 owner2.goOut()
 
+print(f"{owner1.name} is {owner1.age} years old and has a")
 for pet in owner1.petsOwned:
-    pet.intro()
+    print(pet.desc)
 print(f"{owner1.name} is {'outside' if owner1.isOut else 'inside'}.")
 
 
+
+print(f"{owner2.name} is {owner2.age} years old and has the following pets:")
 for pet in owner2.petsOwned:
-    pet.intro()
+    print(pet.desc)
 print(f"{owner2.name} is {'outside' if owner2.isOut else 'inside'}.")
+
 
